273da9db035f391f9da0b4a634c0f4cb8129cbf7
Gary Pu <push05018117@gmail.com>
2018-10-11 15:07:31 -0400

    Update 410.py

--- leetcode/410.py
+++ leetcode/410.py
@@ -1,5 +1,6 @@
 class Solution1(object):
     '''
+    Memoization
     O(mn^2)
     '''
     def splitArray(self, nums, m):
@@ -30,3 +31,38 @@ class Solution1(object):
         if self.cache[start][m] == None:
             self.cache[start][m] = self.helper(nums, start, m)
         return self.cache[start][m]
+
+class Solution2(object):
+    '''
+    Binary search
+    O(Nlog(sum of nums))
+    '''
+    def splitArray(self, nums, m):
+        """
+        :type nums: List[int]
+        :type m: int
+        :rtype: int
+        """
+        left = max(nums) # max split (m == len(nums))
+        right = sum(nums) # min split (m == 1)
+        while left < right:
+            mid = (left+right) // 2
+            numOfSubs = self.getNumOfSubs(nums, mid) # target max sum is mid, calculate minimum number of subarrays needed
+            if numOfSubs > m:
+                left = mid + 1
+            else:
+                right = mid
+        return left
+    
+    def getNumOfSubs(self, nums, targetSum):
+        currSum = 0
+        res = 1
+        i = 0
+        while i < len(nums):
+            if currSum+nums[i] <= targetSum:
+                currSum += nums[i]
+                i += 1
+            else:
+                res += 1
+                currSum = 0
+        return res
