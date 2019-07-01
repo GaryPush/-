ae216144ae71e9927dae026376e1fc20bee5bbe4
Gary Pu <push05018117@gmail.com>
2019-07-01 11:43:59 -0400

    Update 887.py

--- leetcode/887.py
+++ leetcode/887.py
@@ -41,11 +41,11 @@ class Solution2:
     time: O(KlogN) 
     space: O(KN)
     When I have m-1 moves and k-1 eggs, I can check dp[m-1][k-1] floors.
-    But when I get 1 more move and 1 more egg, I drop an egg at floor dp[m-1][k-1]+1, because I know
+    But when I get 1 more move and 1 more egg, I drop the first egg at floor dp[m-1][k-1]+1, because I know
     floors below that can be checked within m-1 moves and k-1 eggs.
     The worst case is that the egg is not broken, because if the egg is broken, the search is done
     since any floor aboved yields broken egg. Therefore we can then start from dp[m-1][k-1]+1 and 
-    check dp[m-1][k] more floors.
+    check dp[m-1][k] more floors without checking the below floor.
     So the maximum floors I can check with m moves and k eggs is dp[m-1][k-1]+dp[m-1][k]+1
     '''
     def superEggDrop(self, K, N):
